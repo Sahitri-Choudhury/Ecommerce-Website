b80a36db9a6fe383d482cafe2519585137e07cc7
Sahitri-Choudhury <126386361+Sahitri-Choudhury@users.noreply.github.com>
2025-02-20 12:56:52 +0530

    Update app.js

--- ecomstore/backend/app.js
+++ ecomstore/backend/app.js
@@ -11,7 +11,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Add headers in order to perform all operation on API
-// Because CORS Thing (Google it if you do not know)
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Header", "*");
